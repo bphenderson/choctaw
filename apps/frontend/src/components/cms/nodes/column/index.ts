4dc8c9b2c3445bf71685ac34e9165f74c85e9af0
bphenderson <bphenderson@gmail.com>
2025-05-14 10:40:19 -0500

    default column 2

--- apps/frontend/src/components/cms/nodes/column/index.ts
+++ apps/frontend/src/components/cms/nodes/column/index.ts
@@ -2,13 +2,14 @@
 // @not-modified => When this line is removed, the "force" parameter of the CLI tool is required to overwrite this file
 import { type ComponentTypeDictionary } from "@remkoj/optimizely-cms-react";
 import DefaultColumnComponent from "./DefaultColumn";
+import DefaultColumn2Component from "./DefaultColumn2";
 import CardColumnComponent from "./CardColumn";
 
 // Build dictionary
 export const NodesColumnFactory: ComponentTypeDictionary = [
   {
     type: "DefaultColumn2",
-    component: DefaultColumnComponent,
+    component: DefaultColumn2Component,
   },
   {
     type: "DefaultColumn",
